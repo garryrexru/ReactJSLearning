4ad047db72d8ffd78875f9d37d532b7abbe6d9db
garryrexru <garryrexru@gmail.com>
2019-11-18 19:37:29 +0700

    Part_5

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Footer() {
+    return (
+        <footer>
+            <h3><font color="#3ac1ef">This is my footer element</font></h3>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+function Header() {
+    return (
+        <nav>
+            <h1>Hello a third time!</h1>
+            <u1>
+                <li>Thing 1</li>
+                <li>Thing 2</li>
+                <li>Thing 3</li>
+            </u1>
+        </nav>
+    )
+}
+
+export default Header
